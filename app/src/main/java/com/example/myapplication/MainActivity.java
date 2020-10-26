f0191934a2324cb4ac7d67b689bb32db6e86c41e
3bimaq24 <lucasisaac957@gmail.com>
2020-10-26 09:40:35 -0400

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView6);
+
+        Glide.with(this).load("https://img.youtube.com/vi/qxXr2CYjHl8/0.jpg").into(imageView);
+    }
+}
